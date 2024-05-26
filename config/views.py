ddd1b1da2d3ceba33148826b6753124567f2803a
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-05-26 17:53:38 +0330

    created single post

--- config/views.py
+++ config/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import redirect, render
 
 from blogs.models import Blog, Category
 
+from assignments.models import About
 
-# from assignments.models import About
+
 # from .forms import RegistrationForm
 # from django.contrib.auth.forms import AuthenticationForm
 # from django.contrib import auth
 
 
 def home(request):
-    categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published')
+    # Fetch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
     context = {
-        'categories': categories,
         'featured_posts': featured_posts,
-        'posts': posts
+        'posts': posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
