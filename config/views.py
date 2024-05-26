b3ffd42a9373ad95d0ec4afacf213aab5882dc4b
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-05-26 13:31:16 +0330

    add categories and add features post and recent posts tp home page

--- config/views.py
+++ config/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 
-# from blogs.models import Blog, Category
+from blogs.models import Blog, Category
+
+
 # from assignments.models import About
 # from .forms import RegistrationForm
 # from django.contrib.auth.forms import AuthenticationForm
@@ -8,4 +10,12 @@ from django.shortcuts import redirect, render
 
 
 def home(request):
-    return render(request, 'home.html',)
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False, status='Published')
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts,
+        'posts': posts
+    }
+    return render(request, 'home.html', context)
