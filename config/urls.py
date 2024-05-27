743564a979399d71f8c7fd70606dc16f3cf2f7ab
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-05-27 10:36:22 +0330

    registration and login and log out functionality

--- config/urls.py
+++ config/urls.py
@@ -30,4 +30,9 @@ urlpatterns = [
     path('blogs/<slug:slug>/', BlogsView.blogs, name='blogs'),
     # Search endpoint
     path('search/', BlogsView.search, name='search'),
+    # user authentication
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
